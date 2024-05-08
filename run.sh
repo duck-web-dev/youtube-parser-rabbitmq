d669a5d38b8634d3b8dc47319545e23352ea34df
duck-web-dev <duck.web.dev@proton.me>
2024-05-08 18:00:32 +0300

    Fix tmux command

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-export LOGGING_LEVEL=INFO;
+export LOGGING_LEVEL=INFO
 
-cd src;
+cd src
 
 # Setup RabbitMQ
 python3 declare.py &&
 
 # Run consumers in screen sessions
-tmux new-session -d -s "db_consumer"		"python3 db_consumer.py" &
-tmux new-session -d -s "parser_consumer"	"PARSER_WORKERS="$PARSER_WORKERS" python3 parser_consumer.py" &;
+tmux new-session -d -s "db_consumer"		"python3 db_consumer.py" &&
+tmux new-session -d -s "parser_consumer"	"PARSER_WORKERS="$PARSER_WORKERS" python3 parser_consumer.py" &&
 
 # For docker not to shut down
 for ((;;)) do sleep infinity; done;
